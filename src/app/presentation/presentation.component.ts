6eb922c024ce9946c66348163fdb3a289efddef5
DaniellaMoran <daniellamoran22@gmail.com>
2023-06-05 09:48:06 +0300

    decided to do presentations manually now

--- src/app/presentation/presentation.component.ts
+++ src/app/presentation/presentation.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { presentationPage, presentations } from '../info';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-presentation',
@@ -14,23 +13,11 @@ export class PresentationComponent implements OnInit {
   presentations = presentations;
   presentationPage = presentationPage;
 
-  constructor(private sanitizer: DomSanitizer) { }
-
   ngOnInit(): void {
-    // document.getElementById("iframe")?.setAttribute("src", this.presentations[this.currentMachine]);
-    // console.log(document.getElementById("iframe")?.getAttribute("src"));
     
     
     setTimeout(() => {
       this.activeMessage = false;
-      // var iframe = document.createElement('iframe');
-      // iframe.onload = function() { alert('myframe is loaded'); }; // before setting 'src'
-      // iframe.classList.add("screen");
-      // console.log(iframe.src);
-      setTimeout(() => {
-        document.getElementById("iframe")?.setAttribute("src",this.presentations[this.currentMachine]) ;
-        // document.getElementById("iframe")?.appendChild(iframe);
-      }, 2000);
     }, this.waitingTime);
   }
 
